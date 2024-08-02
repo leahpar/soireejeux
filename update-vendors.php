8c68c999da65ce0d5d5f97de2ff99feb6306c0f6
Raphaël Bacco <raphael@bacco.fr>
2024-08-02 11:04:44 +0200

    fix script

--- update-vendors.php
+++ update-vendors.php
@@ -4,10 +4,9 @@ $filePath = __DIR__ . '/vendor/easycorp/easyadmin-bundle/src/Orm/EntityRepositor
 
 $content = file_get_contents($filePath);
 
-// Use a regular expression to replace 'private function' with 'protected function'
-$modifiedContent = preg_replace('/private function addOrderClause/', 'protected function addOrderClause', $content);
-$modifiedContent = preg_replace('/final class EntityRepository/', 'class EntityRepository', $content);
+$content = str_replace('private function addOrderClause', 'protected function addOrderClause', $content);
+$content = str_replace('final class EntityRepository', 'class EntityRepository', $content);
 
-file_put_contents($filePath, $modifiedContent);
+file_put_contents($filePath, $content);
 
 echo "Vendor class updated successfully.\n";
